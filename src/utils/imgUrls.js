9e3d24546138853c32f440099ab617b2acf76702
Sarah Hick <sarah0032@hotmail.com>
2020-06-27 18:09:51 +0200

    refactor: Add all projects assets and remove useless files

--- src/utils/imgUrls.js
+++ src/utils/imgUrls.js
@@ -1,11 +1,39 @@
-import aeaThumb from "../assets/img/aea-thumb.png";
-import aeaThumbRetina from "../assets/img/aea-thumb2x.png";
+import aeaThumb from "../assets/img/aea/aea-thumb.png";
+import aeaThumbRetina from "../assets/img/aea/aea-thumb2x.png";
 
-import aeoThumb from "../assets/img/aeo-thumb.png";
-import aeoThumbRetina from "../assets/img/aeo-thumb2x.png";
+import aeaGoods from "../assets/img/aea/aea-goods.png";
+import aeaHome from "../assets/img/aea/aea-home.png";
+import aeaImmo from "../assets/img/aea/aea-immo.png";
+import aeaProperty from "../assets/img/aea/aea-property.png";
 
-import cflThumb from "../assets/img/cfl-thumb.png";
-import cflThumbRetina from "../assets/img/cfl-thumb2x.png";
+import aeoThumb from "../assets/img/aeo/aeo-thumb.png";
+import aeoThumbRetina from "../assets/img/aeo/aeo-thumb2x.png";
+
+import aeoContactList from "../assets/img/aeo/aeo-contact-list.png";
+import aeoEvent from "../assets/img/aeo/aeo-event.png";
+import aeoContact from "../assets/img/aeo/aeo-contact.png";
+import aeoContactMobile from "../assets/img/aeo/aeo-contact-mobile.png";
+
+import cflThumb from "../assets/img/cfl/cfl-thumb.png";
+import cflThumbRetina from "../assets/img/cfl/cfl-thumb2x.png";
+
+import cflContact from "../assets/img/cfl/cfl-contact.png";
+import cflGroup from "../assets/img/cfl/cfl-groupe.jpg";
+import cflHistory from "../assets/img/cfl/cfl-historique.jpg";
+import cflHome from "../assets/img/cfl/cfl-home.jpg";
+import cflNetwork from "../assets/img/cfl/cfl-network.png";
+import cflNumbers from "../assets/img/cfl/cfl-numbers.png";
+import cflProjects from "../assets/img/cfl/cfl-projects.png";
+import cflTimetables from "../assets/img/cfl/cfl-timetables.png";
+import cflTransport from "../assets/img/cfl/cfl-transport.png";
+
+import raiffeisenThumb from "../assets/img/raiffeisen/raiffeisen-thumb.png";
+import raiffeisenThumbRetina from "../assets/img/raiffeisen/raiffeisen-thumb2x.png";
+
+import raiffeisenPersonal from "../assets/img/raiffeisen/raiffeisen-personal-loan.png";
+import raiffeisenSimulator1 from "../assets/img/raiffeisen/raiffeisen-simulator-step1.png";
+import raiffeisenSimulator2 from "../assets/img/raiffeisen/raiffeisen-simulator-step2.png";
+import raiffeisenSimulator3 from "../assets/img/raiffeisen/raiffeisen-simulator-step3.png";
 
 export {
   aeaThumb,
@@ -14,4 +42,27 @@ export {
   aeoThumbRetina,
   cflThumb,
   cflThumbRetina,
+  raiffeisenThumb,
+  raiffeisenThumbRetina,
+  aeaGoods,
+  aeaHome,
+  aeaImmo,
+  aeaProperty,
+  aeoContactList,
+  aeoEvent,
+  aeoContact,
+  aeoContactMobile,
+  cflContact,
+  cflGroup,
+  cflHistory,
+  cflHome,
+  cflNetwork,
+  cflNumbers,
+  cflProjects,
+  cflTimetables,
+  cflTransport,
+  raiffeisenPersonal,
+  raiffeisenSimulator1,
+  raiffeisenSimulator2,
+  raiffeisenSimulator3,
 };
