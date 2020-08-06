5134381e51313733f3b182f1090f856e30f96e1a
Sarah Hick <sarah0032@hotmail.com>
2020-08-06 19:40:02 +0200

    refactor: removing useless folder

--- src/js/loader.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDelayRender from "react-delay-render";
-
-const Loading = () => <div>Loading</div>;
-
-export default ReactDelayRender({ delay: 1000 })(Loading);
