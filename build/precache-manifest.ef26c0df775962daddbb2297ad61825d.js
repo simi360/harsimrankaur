42944d3d1b4366979db8f625302a9408acbf9f81
Harsimran Kaur <infamously13@gmail.com>
2022-11-03 20:39:30 -0400

    First commit of the project

---
+++ build/precache-manifest.ef26c0df775962daddbb2297ad61825d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efbfb0e10fc47029136421619e8e965",
+    "url": "/harsimrankaur/index.html"
+  },
+  {
+    "revision": "16b08ca97c0473053741",
+    "url": "/harsimrankaur/static/css/2.4e603e80.chunk.css"
+  },
+  {
+    "revision": "16b08ca97c0473053741",
+    "url": "/harsimrankaur/static/js/2.8e8ea0eb.chunk.js"
+  },
+  {
+    "revision": "668906fee09be058b29091ed919e5cf1",
+    "url": "/harsimrankaur/static/js/2.8e8ea0eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57afd8f53a39cffb2bf2",
+    "url": "/harsimrankaur/static/js/3.dbe47fd5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/harsimrankaur/static/js/3.dbe47fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0dfc86648331a58ef8a",
+    "url": "/harsimrankaur/static/js/4.4c0a3040.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/harsimrankaur/static/js/4.4c0a3040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3ba7742c06f17df364",
+    "url": "/harsimrankaur/static/js/5.4ea7d8b5.chunk.js"
+  },
+  {
+    "revision": "d543d744a82d85286958",
+    "url": "/harsimrankaur/static/js/main.85b4eed5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/harsimrankaur/static/js/main.85b4eed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980d521e6b160efab64f",
+    "url": "/harsimrankaur/static/js/runtime-main.79a8c684.js"
+  }
+]);
