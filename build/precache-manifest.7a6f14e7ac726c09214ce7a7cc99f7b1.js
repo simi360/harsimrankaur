bb5a97a5670eabc196f921b84b1d16ae20c05cee
Harsimran Kaur <infamously13@gmail.com>
2022-11-09 17:35:12 -0500

    build folder

---
+++ build/precache-manifest.7a6f14e7ac726c09214ce7a7cc99f7b1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfee16d1ea6e4705c5bc2f3a24dd354",
+    "url": "/harsimrankaur/index.html"
+  },
+  {
+    "revision": "16b08ca97c0473053741",
+    "url": "/harsimrankaur/static/css/2.4e603e80.chunk.css"
+  },
+  {
+    "revision": "16b08ca97c0473053741",
+    "url": "/harsimrankaur/static/js/2.8e8ea0eb.chunk.js"
+  },
+  {
+    "revision": "668906fee09be058b29091ed919e5cf1",
+    "url": "/harsimrankaur/static/js/2.8e8ea0eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dfff32af9916a56c79",
+    "url": "/harsimrankaur/static/js/3.8aa0b579.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/harsimrankaur/static/js/3.8aa0b579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67739e65064fb8d05117",
+    "url": "/harsimrankaur/static/js/4.153b888b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/harsimrankaur/static/js/4.153b888b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3ba7742c06f17df364",
+    "url": "/harsimrankaur/static/js/5.4ea7d8b5.chunk.js"
+  },
+  {
+    "revision": "2026fa1c0319d484e8fa",
+    "url": "/harsimrankaur/static/js/main.7cba6605.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/harsimrankaur/static/js/main.7cba6605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c6b8593d426da7fb95",
+    "url": "/harsimrankaur/static/js/runtime-main.2ff69fdb.js"
+  }
+]);
